c36e0c148dd44c955602671c617f0ae4ba7aeef2
riho <AdXenith@users.noreply.github.com>
2023-12-02 09:08:24 +0200

    day one part one

--- Day 1 Trebuchet/app.js
+++ Day 1 Trebuchet/app.js
@@ -1,1 +1,28 @@
-const lines = [];
+var fs = require('fs');
+
+const lines = filename =>
+ fs
+  .readFileSync(filename)
+  .toString('utf-8')
+  .split('\n');
+
+let arr = lines('input.txt');
+let sum = 0;
+
+arr.forEach(line => {
+    let numbers = line.match(/([0-9])+?/g);
+    let secondNumber;
+
+    if (numbers.length === 1) {
+        secondNumber = numbers[0].toString()
+    } else {
+        secondNumber = (numbers[numbers.length - 1]).toString();
+    }
+
+    let calibration =  numbers[0].toString() + secondNumber;
+    //console.log(calibration);
+
+    sum += parseInt(calibration);
+});
+
+console.log("Total Sum: " + sum);
